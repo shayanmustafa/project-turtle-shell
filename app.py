d5a3077267b7f837fb47e342e01f07f151bba74c
Shayan Mustafa <mustafa.shayan9@gmail.com>
2020-07-28 19:17:01 +0500

    button styling

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from tkinter import Tk, Button
+from tkinter import Tk, Button, font
+from tkinter.ttk import *
 from modules.new_racer import EnterRacer
 from modules.racer_statistics import RacerStatistics
 from modules.racertrack import RaceTrack
@@ -9,26 +10,34 @@ from modules.compare_racers import CompareRacers
 app = Tk()
 app.title('Racing Bet')
 app.geometry('700x400')
-app['background']='#002A53'
+app['background']='#2A3132'
+
+# style object
+style = Style()
+style.theme_use('alt')
+style.configure('W.TButton', font =
+               ('Arial CYR', 10), 
+                foreground = 'white', background = '#336B87', borderwidth = '1')
+style.map('W.TButton', background=[('active','#336B87')])                 
 
 # enter new racer
-btnNewRacer = Button(app, text = "Enter New Racer", command = EnterRacer)
+btnNewRacer = Button(app, text = "Enter New Racer", style = 'W.TButton', command = EnterRacer)
 btnNewRacer.grid(row = 0, column = 0, pady = 50, padx = 50)
 
 # enter racer statistics
-btnRacerStatistics = Button(app, text = "Enter Racer Statistics", command = RacerStatistics)
+btnRacerStatistics = Button(app, text = "Enter Racer Statistics", style = 'W.TButton', command = RacerStatistics)
 btnRacerStatistics.grid(row = 0, column = 1, pady = 50, padx = 50)
 
 # enter racetrack information
-btnRaceTrack = Button(app, text = "Enter Racetrack Information", command = RaceTrack)
+btnRaceTrack = Button(app, text = "Enter Racetrack Information", style = 'W.TButton', command = RaceTrack)
 btnRaceTrack.grid(row = 0, column = 2, pady = 50, padx = 50)
 
 # show racer statistics
-btnShowStatistics = Button(app, text = "Show Racer's Statistics", command = ShowStatistics)
+btnShowStatistics = Button(app, text = "Show Racer's Statistics", style = 'W.TButton', command = ShowStatistics)
 btnShowStatistics.grid(row = 1, column = 0, pady = 50, padx = 50)
 
 # compare racers
-btnCompareRacers = Button(app, text = "Compare Racers", command = CompareRacers)
+btnCompareRacers = Button(app, text = "Compare Racers", style = 'W.TButton', command = CompareRacers)
 btnCompareRacers.grid(row = 1, column = 1, pady = 50, padx = 50)
 
 # start app
